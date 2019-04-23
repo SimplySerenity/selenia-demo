0ee6f4e5fdbacbf821a46fd025eeff46c40c8582
SimplySerenity <mcgoughboy@gmail.com>
2019-04-22 17:04:54 -0700

    init commit

---
+++ precache-manifest.94a95ba0f202b900abf2a93a128b786f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ccb0c15bdf9ccd21bc2",
+    "url": "/static/css/main.75174d80.chunk.css"
+  },
+  {
+    "revision": "9ccb0c15bdf9ccd21bc2",
+    "url": "/static/js/main.243d2f9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6f86a441c978c382fd6",
+    "url": "/static/css/2.81ab6a20.chunk.css"
+  },
+  {
+    "revision": "b6f86a441c978c382fd6",
+    "url": "/static/js/2.3ae6d318.chunk.js"
+  },
+  {
+    "revision": "4633f082171ed0d1a3b436ec58ffc65b",
+    "url": "/index.html"
+  }
+];
